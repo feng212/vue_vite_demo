761cd4e2aa1f8ed14235e9747b8a6e543ef3cd46
pengyanfeng <pengyanfeng@wi-stor.com>
2023-03-03 17:43:29 +0800

    init

---
+++ src/components/TextInsert.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from 'vue'
+
+const data=ref(
+    {
+      dog:{
+        name:"dog",
+        call:"汪汪"
+      },
+      msg:"text案例"
+    }
+    )
+</script>
+
+<template>
+<div>
+  <p>{{data.dog}}</p>
+  <p>{{ data.msg}}</p>
+</div>
+</template>
---
+++ src/components/VBind.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from "vue";
+const data=ref({
+  src:'src',
+  href:'href',
+  srcdata1:'https://img0.baidu.com/it/u=525778574,3424229382&fm=253&fmt=auto&app=138&f=JPEG?w=450&h=338',
+  hrefdata1:'www.baidu.com',
+  message:'去百度搜索图片'
+})
+</script>
+<template>
+  <div>
+    <a :href="data.hrefdata1">
+      {{ data.message}}
+      <img :src="data.srcdata1"/>
+    </a>
+
+  </div>
+
+</template>
+
+
+
+
